f8a2225e79fdfb1ce92d0b79aeebfaede20d3781
DangHoangLong <long.711.sg@gmail.com>
2026-04-26 19:29:52 +0700

    Flashing relay

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Tortoise)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.showIcon(IconNames.Tortoise)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Angry)
     music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 262, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Angry)
     pins.digitalWritePin(DigitalPin.P2, 0)
     basic.pause(2000)
     pins.digitalWritePin(DigitalPin.P2, 1)
     basic.showIcon(IconNames.Meh)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.House)
     pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.showIcon(IconNames.House)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(100)
+    }
 })
 basic.showIcon(IconNames.Meh)
